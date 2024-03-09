bc72cb12b538a22327701737f4300a07107e8341
ankushlohiya001 <ankushlohiya2000@gmail.com>
2024-03-09 19:21:05 +0530

    added: some description for fut

--- src/moves.rs
+++ src/moves.rs
@@ -2,6 +2,14 @@ use crate::errors::GameError;
 
 pub use crate::chess_matrix::Pos;
 
+// TODOs
+// move manager can perform moves
+// based on pattern specified in chess,
+//  ie. e2, moving pawn to e2,
+//      Kg4, moving King to g4
+//
+// also records so as to perform undos/redos
+
 pub trait Moving {
     fn possible_moves(&self) -> Vec<Pos>;
 
