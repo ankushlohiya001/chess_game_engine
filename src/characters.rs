f10bac7185992595777f87d8dfa786c0b25d7e14
ankushlohiya001 <ankushlohiya2000@gmail.com>
2024-03-15 16:58:38 +0530

    added: characters symbols:D

---
+++ src/characters.rs
@@ -0,0 +1,6 @@
+pub const King: (char, char) = ('♔', '♚');
+pub const Queen: (char, char) = ('♕', '♛');
+pub const Rook: (char, char) = ('♖', '♜');
+pub const Bishop: (char, char) = ('♗', '♝');
+pub const Knight: (char, char) = ('♘', '♞');
+pub const Pawn: (char, char) = ('♙', '♟');
